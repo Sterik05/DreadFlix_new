1e2476549cc3905830f581d0a82f7652c8ebb9a6
Sterik05 <riccardostefanini67@gmail.com>
2025-01-20 19:22:59 +0100

    navbar e sidebar
    creazione navbar sidebar

--- dread-flix/src/App.vue
+++ dread-flix/src/App.vue
@@ -1,30 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+<nav>
+  <a href="/"><h1>DREAD FLIX</h1></a>
+  <ul class="sidebar">
+    <button @click="hideSidebar()">
+      <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a>
+    </button>
+    <li><RouterLink to="src/views/type/Movies.vue">Movies</RouterLink></li>
+    <li><RouterLink to="src/views/type/TVSeries.vue">Tv Series</RouterLink></li>
+  </ul>
+  <ul> 
+    <li>
+      <a href="/">
+      <RouterLink to="/">
+        <img src="C:\Users\ricca\OneDrive\Desktop\PCTO\progetti finali\DreadFlix\dread-flix\public\dreadflix_logo.webp"  alt="Dread Flix">
+      </RouterLink>
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    </li>
+
+    <button>
+      <RouterLink to="src/components/Search.vue"><svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#e8eaed"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg></RouterLink>
+    </button>
+
+    <button @click="showSidebar()" id="show">
+      <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="70px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a>
+    </button>
+  </ul>
+</nav>    
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  methods: {
+    showSidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      if (sidebar) {
+        sidebar.style.display = sidebar.style.display === 'flex' ? 'none' : 'flex';
+      }
+    },
+    hideSidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      if (sidebar) {
+        sidebar.style.display = sidebar.style.display === 'none' ? 'flex' : 'none';
+      }
+    },
+  },
+};
+</script>
